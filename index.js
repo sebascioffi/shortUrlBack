ff8c2d93abcd4eeb7a3d3e0bb845688cd87c19db
sebascioffi <sebacioffi12@gmail.com>
2024-04-03 19:46:41 -0300

    CorrexionCred

--- index.js
+++ index.js
@@ -29,8 +29,7 @@ app.use(cors({
             return callback(null, origin)
         }
         return callback("Error de CORS origin: " + origin + " No autorizado!")
-    },
-    credentials:true
+    }
 }))
 
 app.use(express.json())
