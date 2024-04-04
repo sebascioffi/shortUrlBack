b390cd74ce1b0c2932bb1e4c73abd06a98921d6a
sebascioffi <sebacioffi12@gmail.com>
2024-04-04 10:39:14 -0300

    commit829

--- utils/tokenManager.js
+++ utils/tokenManager.js
@@ -21,8 +21,7 @@ export const generateRefreshToken = (uid, res) => {
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,     
             secure: !(process.env.MODO === "developer"),
-            expires: new Date(Date.now() + expiresIn * 1000), //esta en milisegundos por eso hay que multiplicarlo por mil.
-            domain: 'localhost'
+            expires: new Date(Date.now() + expiresIn * 1000)//esta en milisegundos por eso hay que multiplicarlo por mil.
         })  //la guardamos en la cookie porque da lo mismo si nos lo roban o no. esto es solo para regenerar otro token.
     } catch (error) {
         console.log(error);
